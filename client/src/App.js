7eebfe176f2df094fd827f27fd225b77e6d3c441
lucasmandirola <95371240+lucasmandirola@users.noreply.github.com>
2022-06-02 14:02:05 -0300

    Auth route and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-import { getPosts } from './redux/actions/posts'
-import memories from './imgs/memories.png';
-import Posts from './Components/Posts/Posts';
-import Form from './Components/Form/Form';
-import useStyles from './styles';
+import React from 'react';
+import { Container } from '@material-ui/core';
 import NavBar from './Components/NavBar/NavBar';
+import Home from './Components/Home/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Auth from './Components/Auth/Auth';
 
 function App() {
-  const classes = useStyles()
-  const dispatch = useDispatch()
-  const [currentId, setCurrentId] = useState(null)
-
-  useEffect(() => {
-    dispatch(getPosts())
-  }, [currentId, dispatch])
-
   return (
-    <Container maxidth='lg'>
-      <NavBar/>
-      <Grow in>
-        <Container>
-          <Grid className={classes.mainContainer} container justify='space-between' alignItems='stretch' spacing={3}>
-            <Grid items xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId}/>
-            </Grid>
-            <Grid items xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId}/>
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <BrowserRouter>
+      <Container maxidth='lg'>
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/auth' element={<Auth/>}/>
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 }
 
--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Toolbar, AppBar, Typography, Button, Avatar } from '@material-ui/core';
 import useStyles from './styles';
 import memories from '../../imgs/memories.png';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-  const classes = useStyles()
+  const classes = useStyles();
+  const user = null;
   return (
     <AppBar className={classes.appBar} position='static' color='inherit'>
-      <Typography className={classes.heading} variant='h2' align='center' >Memories</Typography>
-      <img className={classes.image} src={memories} alt='memories' height='60'/>
+      <div>
+        <Typography component={Link} to='/' className={classes.heading} variant='h2' align='center' >Memories</Typography>
+        <img className={classes.image} src={memories} alt='memories' height='60'/>
+      </div>
+      <Toolbar classname={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+            <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
+            <Button variant='contained' className={classes.logout} color='secondary'>Logout</Button>
+          </div>
+        ) : (
+          <Button component={Link} to='/auth' variant='contained' color='primary'>Sign in</Button>
+        )}
+      </Toolbar>
     </AppBar>
   )
 }
